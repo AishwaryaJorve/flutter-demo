4e8b249786a5294c150305c45e557d90bcaad496
AishwaryaJorve <aishwaryajorve1604@gmail.com>
2024-05-17 22:30:19 +0530

    first commitmy first app with hello message

---
+++ lib/styled_text.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class styledText extends StatelessWidget {
+
+  styledText(this.text, {super.key});
+
+  String text;
+
+  @override
+  Widget build(context) {
+    return Text(
+      text,
+      style: const TextStyle(color: Colors.white, fontSize: 28),
+    );
+  }
+}
